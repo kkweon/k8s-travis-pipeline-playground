079c0d4f2bd56057ae2ce73d5a780e4e1934a984
Mo Kweon <kkweon@google.com>
2019-11-15 21:50:57 -0800

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,17 @@
 #!/usr/bin/env bash
-docker build -t kkweon/multi-client -f ./client/Dockerfile ./client
-docker build -t kkweon/multi-server -f ./server/Dockerfile ./server
-docker build -t kkweon/multi-worker -f ./worker/Dockerfile ./worker
+GIT_SHA=$(git rev-parse HEAD)
+docker build                    \
+-t kkweon/multi-client:latest   \
+-t kkweon/multi-client:$GIT_SHA \
+-f ./client/Dockerfile ./client
+docker build                    \
+-t kkweon/multi-server:latest   \
+-t kkweon/multi-server:$GIT_SHA \
+-f ./server/Dockerfile ./server
+docker build                    \
+-t kkweon/multi-worker:latest   \
+-t kkweon/multi-worker:$GIT_SHA \
+-f ./worker/Dockerfile ./worker
 
 docker push kkweon/multi-client
 docker push kkweon/multi-server
